7dc088809c3cb69e75824c486fa596c2ada544c4
howpdf2-debug <howpdf2@gmail.com>
2026-02-02 16:15:42 +0530

    Implement loadHTML function for dynamic content
    Add async function to load HTML content dynamically.

--- include.js
+++ include.js
@@ -1,1 +1,11 @@
+async function loadHTML(id, file) {
+  const el = document.getElementById(id);
+  if (el) {
+    const res = await fetch(file);
+    el.innerHTML = await res.text();
+  }
+}
+
+loadHTML("header", "includes/header.html");
+loadHTML("footer", "includes/footer.html");
 
