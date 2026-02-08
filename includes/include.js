cde4136e7ee14df43bbcfbcfe103fa2097235600
howpdf2-debug <howpdf2@gmail.com>
2026-02-08 22:15:48 +0530

    Update include.js

--- includes/include.js
+++ includes/include.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("/components/header.html")
+    .then(res => res.text())
+    .then(data => document.getElementById("header").innerHTML = data);
+
+  fetch("/components/footer.html")
+    .then(res => res.text())
+    .then(data => document.getElementById("footer").innerHTML = data);
+});
+
+function toggleTools(){
+  document.getElementById("toolsMenu").classList.toggle("show");
+}
 
